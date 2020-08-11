9ee2620e3c008089781fd9c878b841915294ba28
jrfnl <jrfnl@users.noreply.github.com>
2020-08-11 05:47:55 +0200

    PHP 8.0: required parameters are no longer allowed after optional parameters
    As it already wasn't possible to pass the required parameters without also passing the optional one anyway, I'm removing the default value for the (not so) optional parameters and documenting the expected value to "skip" the parameter in the docblock instead.

--- src/wp-includes/functions.php
+++ src/wp-includes/functions.php
@@ -833,11 +833,11 @@ function wp_extract_urls( $content ) {
  *
  * @global wpdb $wpdb WordPress database abstraction object.
  *
- * @param string      $content Post content. If `null`, the `post_content` field from `$post` is used.
+ * @param string|null $content Post content. If `null`, the `post_content` field from `$post` is used.
  * @param int|WP_Post $post    Post ID or post object.
  * @return null|bool Returns false if post is not found.
  */
-function do_enclose( $content = null, $post ) {
+function do_enclose( $content, $post ) {
 	global $wpdb;
 
 	// @todo Tidy this code and make the debug code optional.
