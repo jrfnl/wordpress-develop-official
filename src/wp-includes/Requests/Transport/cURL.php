f1614c2564afccdc6ac5ee2f2abf1314e97a0fb8
jrfnl <jrfnl@users.noreply.github.com>
2021-08-11 01:24:48 +0200

    Requests: apply patch / PR 500

--- src/wp-includes/Requests/Transport/cURL.php
+++ src/wp-includes/Requests/Transport/cURL.php
@@ -361,7 +361,7 @@ class Requests_Transport_cURL implements Requests_Transport {
 				$data = '';
 			}
 			elseif (!is_string($data)) {
-				$data = http_build_query($data, null, '&');
+				$data = http_build_query($data, '', '&');
 			}
 		}
 
@@ -546,7 +546,7 @@ class Requests_Transport_cURL implements Requests_Transport {
 				$query = $url_parts['query'];
 			}
 
-			$query .= '&' . http_build_query($data, null, '&');
+			$query .= '&' . http_build_query($data, '', '&');
 			$query  = trim($query, '&');
 
 			if (empty($url_parts['query'])) {
