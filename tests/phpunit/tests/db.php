e2fe1b57791002f64c5dc7d9f65e571be6818305
jrfnl <jrfnl@users.noreply.github.com>
2022-08-10 21:17:05 +0200

    Tests_DB: move magic method related tests to a separate class

--- tests/phpunit/tests/db.php
+++ tests/phpunit/tests/db.php
@@ -220,51 +220,6 @@ class Tests_DB extends WP_UnitTestCase {
 	}
 
 	/**
-	 * @ticket 18510
-	 */
-	public function test_wpdb_supposedly_protected_properties() {
-		global $wpdb;
-
-		$this->assertNotEmpty( $wpdb->dbh );
-		$dbh = $wpdb->dbh;
-		$this->assertNotEmpty( $dbh );
-		$this->assertTrue( isset( $wpdb->dbh ) ); // Test __isset().
-		unset( $wpdb->dbh );
-		$this->assertTrue( empty( $wpdb->dbh ) );
-		$wpdb->dbh = $dbh;
-		$this->assertNotEmpty( $wpdb->dbh );
-	}
-
-	/**
-	 * @ticket 21212
-	 */
-	public function test_wpdb_actually_protected_properties() {
-		global $wpdb;
-
-		$new_meta = "HAHA I HOPE THIS DOESN'T WORK";
-
-		$col_meta       = $wpdb->col_meta;
-		$wpdb->col_meta = $new_meta;
-
-		$this->assertNotEquals( $col_meta, $new_meta );
-		$this->assertSame( $col_meta, $wpdb->col_meta );
-	}
-
-	/**
-	 * @ticket 18510
-	 */
-	public function test_wpdb_nonexistent_properties() {
-		global $wpdb;
-
-		$this->assertTrue( empty( $wpdb->nonexistent_property ) );
-		$wpdb->nonexistent_property = true;
-		$this->assertTrue( $wpdb->nonexistent_property );
-		$this->assertTrue( isset( $wpdb->nonexistent_property ) );
-		unset( $wpdb->nonexistent_property );
-		$this->assertTrue( empty( $wpdb->nonexistent_property ) );
-	}
-
-	/**
 	 * Test that an escaped %%f is not altered
 	 *
 	 * @ticket 19861
