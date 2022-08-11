94a1adfea01d24e96e9a10de1403e39502f01a40
jrfnl <jrfnl@users.noreply.github.com>
2022-08-11 19:30:41 +0200

    wpdb: add additional tests for changed code in magic methods
    Add additional tests to safeguard that new `private`/`protected` properties added to the `wpdb` class will be properly protected and inaccessible via the magic methods.
    
    Note: in contrast to the tests which _set_ values, these tests do not use the Mock object, but use the real `$wpdb` object for two reasons:
    1. We're testing that changes are disregarded, so if all is well, these tests will not affect any real property values in the `$wpdb` object.
    2. These tests concern `private` properties which are protected against being changed via the magic methods and the Mock object being a child class does not allow us to test this properly.

--- tests/phpunit/tests/db/magicmethods.php
+++ tests/phpunit/tests/db/magicmethods.php
@@ -405,4 +405,94 @@ final class Tests_DB_MagicMethods extends WP_UnitTestCase {
 
 		$unused = $obj->$name;
 	}
+
+	/**
+	 * Verify that using `isset()` on inaccessible, declared properties always returns `false`.
+	 *
+	 * @ticket 56034
+	 *
+	 * @dataProvider data_magic_methods_declared_inaccessible_properties
+	 *
+	 * @param string $name Property name.
+	 */
+	public function test_magic_isset_inaccessible_property( $name ) {
+		global $wpdb;
+		$this->assertFalse( isset( $wpdb->$name ) );
+	}
+
+	/**
+	 * Verify that any attempt to retrieve the value of an inaccessible, declared property will result
+	 * in an exception being thrown.
+	 *
+	 * @ticket 56034
+	 *
+	 * @dataProvider data_magic_methods_declared_inaccessible_properties
+	 *
+	 * @param string $name Property name.
+	 */
+	public function test_magic_get_inaccessible_property( $name ) {
+		global $wpdb;
+
+		$this->expectException( OutOfBoundsException::class );
+		$this->expectExceptionMessage( 'Inaccessible property: wpdb::$' . $name );
+
+		$unused = $wpdb->$name;
+	}
+
+	/**
+	 * Verify that attempting to write to an inaccessible, declared property will fail with an exception being thrown.
+	 *
+	 * @ticket 56034
+	 *
+	 * @dataProvider data_magic_methods_declared_inaccessible_properties
+	 *
+	 * @param string $name Property name.
+	 */
+	public function test_magic_set_inaccessible_property( $name ) {
+		global $wpdb;
+
+		$this->expectException( OutOfBoundsException::class );
+		$this->expectExceptionMessage( 'Inaccessible property wpdb::$' . $name . ' cannot be set' );
+
+		$wpdb->$name = self::TEST_VALUE_1;
+	}
+
+	/**
+	 * Verify that attempting to unset an inaccessible, declared property will fail.
+	 *
+	 * @ticket 56034
+	 *
+	 * @dataProvider data_magic_methods_declared_inaccessible_properties
+	 *
+	 * @param string $name Property name.
+	 */
+	public function test_magic_unset_inaccessible_property( $name ) {
+		global $wpdb;
+
+		// Get the current value of the property.
+		$refl_prop = new ReflectionProperty( $wpdb, $name );
+		$refl_prop->setAccessible( true );
+		$original_value = $refl_prop->getValue( $wpdb );
+
+		unset( $wpdb->$name );
+
+		// Verify that the property value was not changed.
+		$current_value = $refl_prop->getValue( $wpdb );
+		$refl_prop->setAccessible( false );
+
+		$this->assertSame( $original_value, $current_value );
+	}
+
+	/**
+	 * Data provider.
+	 *
+	 * @return array
+	 */
+	public function data_magic_methods_declared_inaccessible_properties() {
+		return array(
+			'Declared private property: $allow_unsafe_unquoted_parameters' => array( 'allow_unsafe_unquoted_parameters' ),
+			'Declared private property: $arbitrary_props' => array( 'arbitrary_props' ),
+			'Declared private property: $compat_accessible_props' => array( 'compat_accessible_props' ),
+		);
+	}
 }
